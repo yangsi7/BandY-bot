104b81befacd1af979ee941a57277274cb06c7da
Euphotic <simon.yang.ch@gmail.com>
2020-03-20 16:13:30 +0100

    Cleaned up the matlab indicator calculation code; modified the python scripts to acommodate Andy's strategy

---
+++ matscripts/funtions/IngestBinance.m
@@ -0,0 +1,35 @@
+function [ddat,TMW] = IngestCrypto(varargin)
+addpath(genpath('/Users/yangsi/Box Sync/UCLA/MATLAB'));
+A.resample={'m','m15','h','h4','d','w'};
+A.rroot='/home/euphotic_/yangino-bot/';
+A.ValidTimeIndex = timerange(datetime('01-Sep-2000','Locale','en_US'),datetime('01-Jan-2200','Locale','en_US'),'closed');
+A = parse_pv_pairs(A,varargin);
+A.csvpath = [A.rroot,'/pythonBinance/Data/BinchBTCUSDT1h.csv'];
+
+warning('off','MATLAB:table:ModifiedAndSavedVarnames');
+
+format long;
+
+ddat.raw=readtable(A.csvpath);
+ddat.open = table2array(ddat.raw(:,2));
+ddat.high = table2array(ddat.raw(:,3));
+ddat.low = table2array(ddat.raw(:,4));
+ddat.close = table2array(ddat.raw(:,5));
+ddat.volume = table2array(ddat.raw(:,6));
+
+% Millisecond timestamp since 1970-01-01
+ddat.militime = table2array(ddat.raw(:,7));
+% Convert to datenum (second timestamp since 0000-00-00)
+%Convert to datetime
+ddat.datetime = datetime(ddat.militime,'ConvertFrom','posixtime','TimeZone','');
+
+% Create a timetable from minute vector input
+TMW.h = timetable(ddat.open,ddat.high,ddat.low,ddat.close,ddat.volume, ...
+    'VariableNames',{'Open','High','Low','Close','Volume'},'RowTimes',ddat.datetime);
+
+idx=find(TMW.h.Close ==0 | TMW.h.Open ==0);
+if ~isempty(idx); TMW.h{idx,:}=nan; end;
+
+TMW.h = fillmissing(TMW.h,'linear');
+TMW.h = TMW.h(A.ValidTimeIndex,:);
+
---
+++ matscripts/funtions/strategy.m
@@ -0,0 +1,52 @@
+function [long, short] = strategy(tmw,varargin)
+
+A.strat = 'Andy';
+a.adx.th = 17;
+A.rsi.obos = 52;
+A=parse_pv_pairs(A,varargin);
+
+% Initialize signals
+
+% Strat (1) 'yhat_fre'
+if strcmp(A.strat,'Andy')
+   long = false(size(tmw.Close));
+   blong.jma = false(size(tmw.Close));
+   blong.rf = false(size(tmw.Close));
+   blong.adx = false(size(tmw.Close));
+   blong.sar = false(size(tmw.Close));
+   blong.rsi = false(size(tmw.Close));
+   blong.macd = false(size(tmw.Close));
+   blong.vol = false(size(tmw.Close));
+
+   short = false(size(tmw.Close));
+   bshort.jma = false(size(tmw.Close));
+   bshort.rf = false(size(tmw.Close));
+   bshort.adx = false(size(tmw.Close));
+   bshort.sar = false(size(tmw.Close));
+   bshort.rsi = false(size(tmw.Close));
+   bshort.macd = false(size(tmw.Close));
+   bshort.vol = false(size(tmw.Close));
+
+   blong.jma(tmw.djma > 0) = true;
+   blong.rf(tmw.High>tmw.rf_hb & tmw.rf_uwrd > 0) = true;
+   blong.adx(tmw.adx_dip > tmw.adx_dim & tmw.adx > a.adx.th) = true;
+   blong.sar(tmw.sar < tmw.Close) = true;
+   blong.rsi(tmw.rsi_v > A.rsi.obos) = true;
+   blong.macd(tmw.macdH > 0) = true;
+   blong.vol(tmw.Volume > tmw.svol) = true;
+
+   bshort.jma(tmw.djma < 0) = true;
+   bshort.rf(tmw.Low<tmw.rf_lb & tmw.rf_dwrd > 0) = true;
+   bshort.adx(tmw.adx_dip < tmw.adx_dim & tmw.adx > a.adx.th) = true;
+   bshort.sar(tmw.sar > tmw.Close) = true;
+   bshort.rsi(tmw.rsi_v < A.rsi.obos) = true;
+   bshort.macd(tmw.macdH < 0) = true;
+   bshort.vol(tmw.Volume > tmw.svol) = true;
+
+   long(blong.jma & blong.rf & blong.adx & blong.sar ...
+      & blong.rsi & blong.macd & blong.vol) = true;
+   short(bshort.jma & bshort.rf & bshort.adx & bshort.sar ...
+      & bshort.rsi & bshort.macd & bshort.vol) = true;
+end
+
+
